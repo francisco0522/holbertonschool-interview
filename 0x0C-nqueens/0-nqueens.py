e5e6184d48dc008db5b59eba8f1246cf2bc509fe
Francisco <francisco.londono@fromwolf.com>
2021-04-29 12:08:04 -0500

    Adding task

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,60 +1,46 @@
 #!/usr/bin/python3
+"""
+    N queens problem
+"""
 import sys
 
 
-def queens(n):
-    paths = []
-    safeZone = set()
-    for column in range(n):
-        paths.append([0, column])
-        safeZone.add(column)
-
-    track = []
-
-    while paths:
-        [row, column] = paths.pop(0)
-        while track and (row < track[0][0]):
-            track.pop(0)
-        if track and (row == track[0][0]):
-            track[0] = [row, column]
-        else:
-            track.insert(0, [row, column])
-
-        nextRow = row + 1
-
-        killZone = set()
-        for (r, c) in track:
-            killZone.add(c)
-            distance = nextRow - r
-            if c - distance >= 0:
-                killZone.add(c - distance)
-            if c + distance < n:
-                killZone.add(c + distance)
-
-        localSafeZone = safeZone.difference(killZone)
-        if not localSafeZone:
-            if nextRow == n:
-                copy = track.copy()
-                copy.reverse()
-                print(copy, flush=True)
-            track.pop(0)
-        else:
-            localSafeZone = list(localSafeZone)
-            localSafeZone.reverse()
-            for position in localSafeZone:
-                paths.insert(0, [nextRow, position])
+def check_pos(s, pos):
+    for q in s:
+        if q[1] == pos[1]:
+            return False
+        if (q[0] + q[1]) == (pos[0] + pos[1]):
+            return False
+        if (q[0] - q[1]) == (pos[0] - pos[1]):
+            return False
+    return True
 
 
-if __name__ == '__main__':
+def mv_q(r, n, s):
+    if (r == n):
+        print(s)
+    else:
+        for col in range(n):
+            pos = [r, col]
+            if check_pos(s, pos):
+                s.append(pos)
+                mv_q(r + 1, n, s)
+                s.remove(pos)
+
+
+if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+        print('Usage: nqueens N')
         exit(1)
     try:
         n = int(sys.argv[1])
-    except:
-        print("N must be a number")
+    except BaseException:
+        print('N must be a number')
         exit(1)
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
+    if n < 4:
+        print('N must be at least 4')
         exit(1)
-    queens(n)
+
+    s = []
+
+    mv_q(0, n, s)
